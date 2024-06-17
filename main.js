651e97c9ec23b7a8cf71be805df585533b22a91c
kbrainard1 <47262866+kbrainard1@users.noreply.github.com>
2024-06-17 07:06:35 -0400

    carousel

--- main.js
+++ main.js
@@ -20,8 +20,8 @@ window.onclick = function (event) {
 }
 
 function prevImage() {
-      carouselIndex--;
-      displayImage();
+    carouselIndex--;
+    displayImage();
 }
 
 function nextImage() {
@@ -31,9 +31,43 @@ function nextImage() {
 
 function displayImage() {
     var nextImage = carouselGallery.children[carouselIndex].getAttribute("full_size");
-    //document.querySelector(".modal_content").style.backgroundImage = "url('" + nextImage + "')";
     document.querySelector(".modal_image").src = nextImage;
+    document.querySelector(".dummy_image").src = nextImage;
+    document.querySelector(".modal_image").style.visibility = "hidden";
     checkBounds();
+}
+
+// "Scale this image to at most 70% of the page in terms of width/height,
+// and then set the outer div to those dimensions" is not easily expressed
+// in CSS in both firefox and chromium.
+// Also needs a dummy image to prevent the size from "sticking" across images
+function resize() {
+    const windowWidth = window.innerWidth * 0.7;
+    const windowHeight = window.innerHeight * 0.7;
+    var modalImage =  document.querySelector(".modal_image");
+    var dummyImage =  document.querySelector(".dummy_image");
+    const imageWidth = dummyImage.width;
+    const imageHeight = dummyImage.height;
+
+    const widthFraction = windowWidth / imageWidth;
+    const heightFraction = windowHeight / imageHeight;
+    var adjustedWidth = imageWidth;
+    var adjustedHeight = imageHeight;
+    if (widthFraction < 1 || heightFraction < 1) {
+        if (widthFraction < heightFraction) {
+            adjustedWidth = imageWidth * widthFraction;
+            adjustedHeight = imageHeight * widthFraction;
+        } else {
+            adjustedWidth = imageWidth * heightFraction;
+            adjustedHeight = imageHeight * heightFraction;
+        }
+    }
+
+    modalImage.style.width = adjustedWidth + "px";
+    modalImage.style.height = adjustedHeight + "px";
+    document.querySelector(".modal_content").style.width = adjustedWidth + "px";
+    document.querySelector(".modal_content").style.height = adjustedHeight + "px";
+    modalImage.style.visibility = "";
 }
 
 function checkBounds() {
