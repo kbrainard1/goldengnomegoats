9c108623db11156577084bef2d3abde35d109681
kbrainard1 <47262866+kbrainard1@users.noreply.github.com>
2024-06-16 23:39:56 -0400

    progress on carousel

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-function displayModal(newImage) {
-    document.querySelector(".modal_image").src = newImage;
+var carouselThumb = 0;
+var carouselGallery;
+
+function displayModal(index, galleryId) {
+    carouselIndex = index;
+    carouselGallery = document.getElementById(galleryId);
+    displayImage();
     document.querySelector(".modal").style.display = "block";
 }
 
 function closeModal() {
+    carouselIndex = 0;
     document.querySelector(".modal").style.display = "none";
 }
 
@@ -11,4 +17,42 @@ window.onclick = function (event) {
     if (event.target == document.querySelector(".modal")) {
         closeModal();
     }
+}
+
+function prevImage() {
+      carouselIndex--;
+      displayImage();
+}
+
+function nextImage() {
+    carouselIndex++;
+    displayImage();
+}
+
+function displayImage() {
+    var nextImage = carouselGallery.children[carouselIndex].getAttribute("full_size");
+    document.querySelector(".modal_content").style.backgroundImage = "url('" + nextImage + "')";
+    checkBounds();
+}
+
+function checkBounds() {
+    var numChildren = carouselGallery.childElementCount;
+    if (carouselIndex <= 0) {
+        hide("prev_image");
+    } else {
+        show("prev_image");
+    }
+    if (carouselIndex >= numChildren - 1) {
+        hide("next_image");
+    } else {
+        show("next_image");
+    }
+}
+
+function hide(elemClass) {
+    document.querySelector("." + elemClass).className = elemClass + " inline hidden";
+}
+
+function show(elemClass) {
+    document.querySelector("." + elemClass).className = elemClass;
 }
